b76991e2c759e7b4cad0484acf8d40d92ef1fc1f
Scott <scottjcraig2@gmail.com>
2019-11-05 09:25:20 -0500

    working children

--- src/EBook.java
+++ src/EBook.java
@@ -1,6 +1,9 @@
 public class EBook extends Book implements Medium {
+
     EBook(){super();}
-    
+    EBook(String title, String description, String author, String subject, int copies, String genre, int releaseYear, int stars, boolean comingSoon, int ISBN){
+        super(title,description, author,subject,copies,genre,releaseYear, stars,comingSoon,ISBN);
+    }
    @Override
     public String toStringList() {
         return "EBook," + title + "," + description + "," + author + "," + subject + "," + copies + "," + genre + "," + releaseYear + "," + stars + "," + comingSoon + "," + ISBN;
