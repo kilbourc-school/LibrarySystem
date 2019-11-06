b9acb2e66b8bfcd30796dbe17f74718e157f4bbe
mrd1 <mrd1@email.sc.edu>
2019-11-06 16:02:27 -0500

    comments on ratings

--- src/Ratings.java
+++ src/Ratings.java
@@ -3,27 +3,41 @@ public class Ratings {
     private String comment;
 
     public Ratings(int rating, String comment) {
+        /*
+        attributes
+         */
         this.rating = rating;
         this.comment = comment;
     }
-
+     /*
+     getter for get rating
+      */
     public int getRating() {
         return rating;
     }
-
+    /*
+    setter for setRating
+     */
     public void setRating(int rating) {
         this.rating = rating;
     }
-
+    /*
+    getter for getComment
+     */
     public String getComment() {
         return comment;
     }
-
+     /*
+     setter for setComment
+      */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
     @Override
+    /*
+    toString method to print objects as strings
+     */
     public String toString() {
         return "rating: " + rating + " comment:" + comment;
     }
