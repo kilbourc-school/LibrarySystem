83ee00ba8f2739bc5b3959ffe36577c61aabce36
Scott <scottjcraig2@gmail.com>
2019-11-06 15:00:23 -0500

    addcopies not working

--- src/LibraryDriver.java
+++ src/LibraryDriver.java
@@ -52,7 +52,7 @@ public class LibraryDriver {
             while (login && currentLibrary.getCurrentUser().isAdmin()) {
                 System.out.println("\n\nWelcome  Administrator " + currentLibrary.getCurrentUser().getName() + ",\nWhat would you like to do?\n");
                 System.out.println("1: Add media\n2: See all accounts\n3: Search by title\n4: Search by genre" +
-                        "\n5: Display all books\n6: Save to file\n7: Add an account\n8:Add Copies to existing book");
+                        "\n5: Display all books\n6: Save to file\n7: Add an account\n8:Checkout\n9:Checkin\n10:change copies");
                 System.out.println("0: Logout");
 
                 int input = keyboard.nextInt();
@@ -107,7 +107,7 @@ public class LibraryDriver {
                         System.out.println("Invalid Input");
                     }
 
-                }else if (input == 8){
+                }else if (input == 10){
                     System.out.println("Enter the book title");
                     String title = keyboard.nextLine();
                     System.out.println("Please enter the amount the copies changes by (+/-):");
