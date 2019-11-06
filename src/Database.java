c4c4b2821236efe5e5f5c963c73ccb2973d47573
Scott <scottjcraig2@gmail.com>
2019-11-06 16:38:56 -0500

    Sting converted

--- src/Database.java
+++ src/Database.java
@@ -63,13 +63,13 @@ public class Database {
                 }
                 if (lineArray[0].equals("Book")) {
 
-                    getMediaDatabase().add(new Book(lineArray[1], lineArray[2], lineArray[3], lineArray[4], Integer.parseInt(lineArray[5]), lineArray[6], Integer.parseInt(lineArray[7]), Integer.parseInt(lineArray[8]), Boolean.parseBoolean(lineArray[9]), current, wait, rating, Integer.parseInt(lineArray[13])));
+                    getMediaDatabase().add(new Book(lineArray[1], lineArray[2], lineArray[3], lineArray[4], Integer.parseInt(lineArray[5]), lineArray[6], Integer.parseInt(lineArray[7]), Integer.parseInt(lineArray[8]), Boolean.parseBoolean(lineArray[9]), current, wait, rating, lineArray[13]));
                 } else if (lineArray[0].equals("AudioBook")) {
                     getMediaDatabase().add(new AudioBook(lineArray[1], lineArray[2], lineArray[3], lineArray[4], Integer.parseInt(lineArray[5]), lineArray[6], Integer.parseInt(lineArray[7]), Integer.parseInt(lineArray[8]), Boolean.parseBoolean(lineArray[9]), current, wait, rating, lineArray[13]));
                 } else if (lineArray[0].equals("DVD")) {
                     getMediaDatabase().add(new DVD(lineArray[1], lineArray[2], lineArray[3], lineArray[4], Integer.parseInt(lineArray[5]), lineArray[6], Integer.parseInt(lineArray[7]), Integer.parseInt(lineArray[8]), Boolean.parseBoolean(lineArray[9]), current, wait, rating));
                 } else if (lineArray[0].equals("EBook")) {
-                    getMediaDatabase().add(new EBook(lineArray[1], lineArray[2], lineArray[3], lineArray[4], Integer.parseInt(lineArray[5]), lineArray[6], Integer.parseInt(lineArray[7]), Integer.parseInt(lineArray[8]), Boolean.parseBoolean(lineArray[9]), current, wait, rating, Integer.parseInt(lineArray[13])));
+                    getMediaDatabase().add(new EBook(lineArray[1], lineArray[2], lineArray[3], lineArray[4], Integer.parseInt(lineArray[5]), lineArray[6], Integer.parseInt(lineArray[7]), Integer.parseInt(lineArray[8]), Boolean.parseBoolean(lineArray[9]), current, wait, rating, lineArray[13]));
                 } else if (lineArray[0].equals("Magazine")) {
                     getMediaDatabase().add(new Magazine(lineArray[1], lineArray[2], lineArray[3], lineArray[4], Integer.parseInt(lineArray[5]), lineArray[6], Integer.parseInt(lineArray[7]), Integer.parseInt(lineArray[8]), Boolean.parseBoolean(lineArray[9]), current, wait,rating, Integer.parseInt(lineArray[13]),Integer.parseInt(lineArray[14])));
                 }
--- src/EBook.java
+++ src/EBook.java
@@ -6,17 +6,13 @@ public class EBook extends Book {
         super();
     }
 
-    public EBook(String title, String description, String author, String subject, int copies, String genre, int releaseYear, int stars, boolean comingSoon, LinkedList<iDandCopies> currentBorrowerID, LinkedList<iDandCopies> waitListBorrowerIDs, LinkedList<Ratings> ratings, int ISBN) {
+    public EBook(String title, String description, String author, String subject, int copies, String genre, int releaseYear, int stars, boolean comingSoon, LinkedList<iDandCopies> currentBorrowerID, LinkedList<iDandCopies> waitListBorrowerIDs, LinkedList<Ratings> ratings, String ISBN) {
         super(title, description, author, subject, copies, genre, releaseYear, stars, comingSoon, currentBorrowerID, waitListBorrowerIDs, ratings, ISBN);
     }
 
     /*
             Returns ISBN Value
              */
-    public int getISBN() {
-        return ISBN;
-    }
-
     /*
     Converts objects into strings
      */
