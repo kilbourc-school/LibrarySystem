e5a7d6434970ca1e6d995abc4e941630ec2eaac9
Scott <scottjcraig2@gmail.com>
2019-11-20 14:53:48 -0500

    borrower test

--- src/BorrowerTest.java
+++ src/BorrowerTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.*;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,10 +8,23 @@ class BorrowerTest {
 
     @BeforeAll
     static void oneTimeSetUp() {
+
+
     }
 
     @BeforeEach
     void setUp() {
+        String name = "Sarah";
+         LocalDate birthday = LocalDate.of(1959, 1, 1);
+         Long ID=234564L;
+         int phoneNum=2341564;
+       String email="user@user";
+         String password="user";
+         String street1="Main Street", city="Anytown", state="SC";
+         int zip=29210;
+         double fines=0.0;
+
+        AdultBorrow user = new AdultBorrow(name,ID,birthday,phoneNum,street1,city,state,zip,email,password,false,fines);
     }
 
     @AfterAll
@@ -24,6 +38,18 @@ class BorrowerTest {
 
     @Test
     void getPhoneNum() {
+        String name = "Sarah";
+        LocalDate birthday = LocalDate.of(1959, 1, 1);
+        Long ID=234564L;
+        int phoneNum = 2341564;
+        String email="user@user";
+        String password="user";
+        String street1="Main Street", city="Anytown", state="SC";
+        int zip=29210;
+        double fines=0.0;
+
+        AdultBorrow user = new AdultBorrow(name,ID,birthday,phoneNum,street1,city,state,zip,email,password,false,fines);
+        assertEquals( phoneNum , user.getPhoneNum());
     }
 
     @Test
