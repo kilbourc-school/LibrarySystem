59d37429545a334ad27fee387993c6fd6c6c0510
Connor <kilbourc@email.sc.edu>
2019-11-20 14:27:38 -0500

    borrower test

---
+++ src/BorrowerTest.java
@@ -0,0 +1,88 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BorrowerTest {
+
+
+    @BeforeAll
+    static void oneTimeSetUp() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterAll
+    static void oneTimeTearDown() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    @Test
+    void getPhoneNum() {
+    }
+
+    @Test
+    void getStreet1() {
+    }
+
+    @Test
+    void getCity() {
+    }
+
+    @Test
+    void getState() {
+    }
+
+    @Test
+    void getZip() {
+    }
+
+    @Test
+    void isAdmin() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getFines() {
+    }
+
+    @Test
+    void getBirthday() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void checkAge() {
+    }
+
+    @Test
+    void checkAgeYear() {
+    }
+
+    @Test
+    void getBorrowLimit() {
+    }
+
+    @Test
+    void toStringList() {
+    }
+}
