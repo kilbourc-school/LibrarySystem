cae21ae33c6fd71c504e21c753948a64316d4b68
Connor <kilbourc@email.sc.edu>
2019-11-20 15:26:20 -0500

    borrower test fix

--- src/LibraryTest.java
+++ src/LibraryTest.java
@@ -1,39 +1,53 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-
 import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
 
 
-
     @org.junit.jupiter.api.Test
     void getMediaFromTitle() {
         Library currentLibrary = new Library();
-currentLibrary.readInMedia();
-Media test = currentLibrary.getMediaFromTitle("The Cat in the Hat");
-assertEquals( test.getTitle(),"The Cat in the Hat");
+        currentLibrary.readInMedia();
+        Media testMedia = currentLibrary.getMediaFromTitle("The Cat in the Hat");
+        assertEquals(testMedia.getTitle(), "The Cat in the Hat");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMediaFromTitleNotInLibrary() {
+        Library currentLibrary = new Library();
+        currentLibrary.readInMedia();
+        Media testMedia = currentLibrary.getMediaFromTitle("The Cat in the Hat");
+        assertNotEquals("The Cat in the Hat but not correct", testMedia.getTitle());
     }
 
     @org.junit.jupiter.api.Test
     void TestCorrectVerifyAccount() {
         Library currentLibrary = new Library();
-        AdultBorrow adultAcc = new AdultBorrow("name",1L, LocalDate.now(),1,"str", "cty","st",29229,
-                "e@e","pw", false,0.0);
+        AdultBorrow adultAcc = new AdultBorrow("name", 1L, LocalDate.now(), 1, "str", "cty", "st", 29229,
+                "e@e", "pw", false, 0.0);
         currentLibrary.addAdultAccount(adultAcc);
-        Boolean check = currentLibrary.verifyAccount(adultAcc.getEmail(), adultAcc.getPassword());
-        assertEquals(true,check);
+        Boolean checkVerify = currentLibrary.verifyAccount(adultAcc.getEmail(), adultAcc.getPassword());
+        assertEquals(true, checkVerify);
     }
+
+    @org.junit.jupiter.api.Test
+    void TestFalseVerifyAccount() {
+        Library currentLibrary = new Library();
+        AdultBorrow adultAcc = new AdultBorrow("name", 1L, LocalDate.now(), 1, "str", "cty", "st", 29229,
+                "e@e", "pw", false, 0.0);
+        currentLibrary.addAdultAccount(adultAcc);
+        Boolean checkVerify = currentLibrary.verifyAccount(adultAcc.getEmail() + "wrong email", adultAcc.getPassword());
+        assertNotEquals(true, checkVerify);
+    }
+
     @org.junit.jupiter.api.Test
     void getCurrentUser() {
         Library currentLibrary = new Library();
-        AdultBorrow adultAcc = new AdultBorrow("name",1L, LocalDate.now(),1,"str", "cty","st",29229,
-                "e@e","pw", false,0.0);
+        AdultBorrow adultAcc = new AdultBorrow("name", 1L, LocalDate.now(), 1, "str", "cty", "st", 29229,
+                "e@e", "pw", false, 0.0);
         currentLibrary.addAdultAccount(adultAcc);
-        Boolean check = currentLibrary.verifyAccount(adultAcc.getEmail(), adultAcc.getPassword());
-        if(adultAcc.equals(currentLibrary.getCurrentUser())) assertTrue(true);
-    assertEquals(adultAcc,currentLibrary.getCurrentUser());
+        currentLibrary.verifyAccount(adultAcc.getEmail(), adultAcc.getPassword());
+        assertEquals(adultAcc, currentLibrary.getCurrentUser());
     }
 
 }
