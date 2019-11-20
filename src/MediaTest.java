4c6362ff1fb26a4f2cddf39856fea9cb545f0200
mrd1 <mrd1@email.sc.edu>
2019-11-20 15:27:00 -0500

    borrower test fix

--- src/MediaTest.java
+++ src/MediaTest.java
@@ -1,9 +1,22 @@
 import org.junit.jupiter.api.*;
 
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MediaTest {
-
+    String title = "The Cat in the Hat";
+    String description = "cats wear hats";
+    String author = "seuss";
+    String subject = "kiddie";
+    int copies = 3;
+    String genre = "kiddo";
+    int releaseYear = 1974;
+    boolean comingSoon = false;
+    LinkedList<iDandCopies> currentBorrowerID;
+    LinkedList<iDandCopies> waitListBorrowerIDs;
+    LinkedList<Ratings> ratings;
+    String ISBN = "1927345";
     @AfterAll
     static void oneTimeTearDown() {
     }
@@ -18,11 +31,17 @@ class MediaTest {
 
     @BeforeEach
     void setUp() {
-    }
 
+        Book book = new Book(title,description,author,subject,copies,genre,releaseYear, comingSoon, currentBorrowerID,waitListBorrowerIDs,ratings, ISBN);
+    }
 
     @Test
     void getDueDate() {
+        LinkedList<iDandCopies> currentBorrowerID = new LinkedList<iDandCopies>();
+        LinkedList<iDandCopies> waitListBorrowerIDs = new LinkedList<iDandCopies>();
+        LinkedList<Ratings> ratings = new LinkedList<Ratings>();
+        Book book = new Book (title,description,author,subject,copies,genre,releaseYear,comingSoon,ISBN);
+        assertEqual(book.getDueDate(),localDate.now );
     }
 
     @Test
@@ -31,34 +50,73 @@ class MediaTest {
 
     @Test
     void getCheckoutLimit() {
+        LinkedList<iDandCopies> currentBorrowerID = new LinkedList<iDandCopies>();
+        LinkedList<iDandCopies> waitListBorrowerIDs = new LinkedList<iDandCopies>();
+        LinkedList<Ratings> ratings = new LinkedList<Ratings>();
+        Book book = new Book (title,  description,  author,  subject,  copies,  genre,  releaseYear, comingSoon,ISBN);
+        assertEqual();
     }
 
     @Test
     void getCurrentBorrowerID() {
+        LinkedList<iDandCopies> currentBorrowerID = new LinkedList<iDandCopies>();
+        LinkedList<iDandCopies> waitListBorrowerIDs = new LinkedList<iDandCopies>();
+        LinkedList<Ratings> ratings = new LinkedList<Ratings>();
+        Book book = new Book (title,description,author,subject,copies,genre,releaseYear,comingSoon,ISBN);
+        assertEqual(book.getCurrentBorrowerID(),);
     }
 
     @Test
     void checkInMedia() {
+        LinkedList<iDandCopies> currentBorrowerID = new LinkedList<iDandCopies>();
+        LinkedList<iDandCopies> waitListBorrowerIDs = new LinkedList<iDandCopies>();
+        LinkedList<Ratings> ratings = new LinkedList<Ratings>();
+        Book book = new Book ( title,  description,  author,  subject,  copies,  genre,  releaseYear, comingSoon,ISBN);
     }
 
     @Test
     void getTitle() {
+        LinkedList<iDandCopies> currentBorrowerID = new LinkedList<iDandCopies>();
+        LinkedList<iDandCopies> waitListBorrowerIDs = new LinkedList<iDandCopies>();
+        LinkedList<Ratings> ratings = new LinkedList<Ratings>();
+        Book book = new Book ( title,  description,  author,  subject,  copies,  genre,  releaseYear, comingSoon,ISBN);
+        assertEqual(book.getTitle(),"The Cat in the Hat");
     }
 
     @Test
     void getAuthor() {
+        LinkedList<iDandCopies> currentBorrowerID = new LinkedList<iDandCopies>();
+        LinkedList<iDandCopies> waitListBorrowerIDs = new LinkedList<iDandCopies>();
+        LinkedList<Ratings> ratings = new LinkedList<Ratings>();
+        Book book = new Book ( title,  description,  author,  subject,  copies,  genre,  releaseYear, comingSoon,ISBN);
+        assertEqual(book.getTitle(),"seuss");
     }
 
     @Test
     void getCopies() {
+        LinkedList<iDandCopies> currentBorrowerID = new LinkedList<iDandCopies>();
+        LinkedList<iDandCopies> waitListBorrowerIDs = new LinkedList<iDandCopies>();
+        LinkedList<Ratings> ratings = new LinkedList<Ratings>();
+        Book book = new Book ( title,  description,  author,  subject,  copies,  genre,  releaseYear, comingSoon,ISBN);
+        assertEqual(book.getCopies(),3);
     }
 
     @Test
     void getGenre() {
+        LinkedList<iDandCopies> currentBorrowerID = new LinkedList<iDandCopies>();
+        LinkedList<iDandCopies> waitListBorrowerIDs = new LinkedList<iDandCopies>();
+        LinkedList<Ratings> ratings = new LinkedList<Ratings>();
+        Book book = new Book ( title,  description,  author,  subject,  copies,  genre,  releaseYear, comingSoon,ISBN);
+        assertEqual(book.getGenre(),"kiddo");
     }
 
     @Test
     void getAvailability() {
+        LinkedList<iDandCopies> currentBorrowerID = new LinkedList<iDandCopies>();
+        LinkedList<iDandCopies> waitListBorrowerIDs = new LinkedList<iDandCopies>();
+        LinkedList<Ratings> ratings = new LinkedList<Ratings>();
+        Book book = new Book ( title,  description,  author,  subject,  copies,  genre,  releaseYear, comingSoon,ISBN);
+        assertEqual(book.getAvailability(),);
     }
 
     @Test
